c86cc28ea732d89b55bd088123a7153c96cec040
eukjin <wjchang8028@naver.com>
2022-04-12 22:22:35 +0900

    arraylist 정렬

---
+++ src/collections/collections.java
@@ -0,0 +1,27 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class collections {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer>list = new ArrayList<Integer>();
+		
+		list.add(10);
+		list.add(20);
+		list.add(50);
+		list.add(30);
+		
+		System.out.println(list);
+		
+		Collections.sort(list); //collections.sort() 정렬
+		System.out.println(list);
+		
+		Collections.sort(list,Collections.reverseOrder()); // 역정렬
+		System.out.println(list);
+
+	}
+
+}
