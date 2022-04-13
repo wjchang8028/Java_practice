de744c01b29d0cbf7fd90372e149907661ef501a
eukjin <wjchang8028@naver.com>
2022-04-13 22:27:18 +0900

    generics

--- src/collections/map.java
+++ src/collections/map.java
@@ -11,7 +11,8 @@ public class map {
 		//dictionary 형태
 		map.put("돈까스", 3000);
 		map.put("볶음밥",5000);
-		
+		map.put("돈까스",6500); //같은 키값은 마지막값으로 갱신
+		//map.put(3000,5000);	// 제네릭스가 맞지 않기 때문에 오류	
 		System.out.println(map);
 		
 
