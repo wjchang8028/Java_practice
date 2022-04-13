b1cea27c6fb20fcbf2705f534da8651de0691422
eukjin <wjchang8028@naver.com>
2022-04-13 22:25:08 +0900

    map 사용법

---
+++ src/collections/map.java
@@ -0,0 +1,20 @@
+package collections;
+
+import java.util.HashMap;
+
+public class map {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<String, Integer>map = new HashMap<String, Integer>();
+		
+		//dictionary ÇüÅÂ
+		map.put("µ·±î½º", 3000);
+		map.put("ººÀ½¹ä",5000);
+		
+		System.out.println(map);
+		
+
+	}
+
+}
