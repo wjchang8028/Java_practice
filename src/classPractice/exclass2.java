26e7a7f8967fe575b22711077eea8419308b2aa2
eukjin <wjchang8028@naver.com>
2022-04-18 21:19:46 +0900

    클래스선언

---
+++ src/classPractice/exclass2.java
@@ -0,0 +1,20 @@
+package classPractice;
+
+class animal {
+	public static void main(String[] args) {
+		System.out.println("µ¿¹°?");
+	}
+}
+
+public class exclass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		animal cat, dog;
+
+		cat = new animal();
+		dog = new animal();
+
+	}
+
+}
