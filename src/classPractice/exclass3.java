1e137d0d2a320b068a629539ee3329befdec1a55
eukjin <wjchang8028@naver.com>
2022-04-18 21:22:35 +0900

    static 활용

---
+++ src/classPractice/exclass3.java
@@ -0,0 +1,23 @@
+package classPractice;
+
+public class exclass3 {
+	
+	public String os;
+	public Long memory;
+	public Long pixel;
+	
+	public static void call(String number) {
+		System.out.println("≈Î»≠");
+	}
+	
+	public void takePic() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String number = "010-8777-6666";
+		call(number);
+	}
+
+}
