1def551a153a1b87dd22d66fb5891f60597c2e65
Chung woo jin <wjchang8028@naver.com>
2022-05-04 23:59:26 +0900

    백준1157 문자열 내 문자 최대갯수

---
+++ src/algorithms/bj1157.java
@@ -0,0 +1,31 @@
+package algorithms;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class bj1157 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String str = scan.next();
+
+		str = str.toUpperCase();
+
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+
+		for (int i = 0; i < str.length(); i++) {
+			if (!map.containsKey(str.charAt(i))) {
+				map.put(str.charAt(i), 1);
+			} else {
+				map.put(str.charAt(i), map.get(str.charAt(i)) + 1);
+			}
+		}
+		System.out.println(map);
+
+		int num = Collections.max(map.values());
+		
+
+	}
+
+}
