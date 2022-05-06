1585b9214cdf3a14a07d0e25630546a617ab4c72
Chung woo jin <wjchang8028@naver.com>
2022-05-06 23:32:13 +0900

    arraylist

---
+++ src/classPractice/ex22.java
@@ -0,0 +1,13 @@
+package classPractice;
+
+import java.util.ArrayList;
+
+public class ex22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> order = new ArrayList<String>();
+		order.add("hi");
+	}
+
+}
